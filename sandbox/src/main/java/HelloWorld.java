f99d41268772696de5746e6a891b682f205fece1
akoscynich <a.koscynich@mygento.ru>
2018-12-28 00:27:45 +0300

    Расстояние

--- sandbox/src/main/java/HelloWorld.java
+++ sandbox/src/main/java/HelloWorld.java
@@ -1,21 +1,18 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        //Point p1 = new Point(7.1, 2.2);
-        //Point p2 = new Point(13.3, 4.4);
-        Point p = new Point();
+        Point p1 = new Point(7.1, 2.2);
+        Point p2 = new Point(13.3, 4.4);
 
         //System.out.println("Hello World!");
         //System.out.println("Расстояние = " + distance(p1, p2));
 
-        System.out.println("Расстояние = " + p.distance());
+        System.out.println("Расстояние от точки с координатами " + p1.a+ ", " + p1.b + " до точки " + p2.a + ", " + p2.b + " = " + Point.distance(p1, p2));
 
     }
 
-
     //public static double distance(Point p1, Point p2){
-    //    return Math.sqrt(Math.pow((p1.a-p2.a),2)*Math.pow((p1.b-p2.b),2));
+    //    return Math.sqrt(Math.pow((p1.a-p2.a),2)+Math.pow((p1.b-p2.b),2));
     //}
-
 
 }
--- sandbox/src/main/java/Point.java
+++ sandbox/src/main/java/Point.java
@@ -3,18 +3,17 @@ public class Point {
     public double a;
     public double b;
 
-    Point p1 = new Point(a, b);
-    Point p2 = new Point(a, b);
-
     public Point(double a, double b){
         this.a = a;
         this.b = b;
     }
 
-    /*public Point(){
+    /*public double distance(Point p1){
+        return Math.sqrt(Math.pow((this.a-p1.a),2)+Math.pow((this.b-p1.b),2));
     }*/
 
-    public double distance(){
-        return Math.sqrt(Math.pow((p1.a-p2.a),2)*Math.pow((p1.b-p2.b),2));
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow((p1.a-p2.a),2)+Math.pow((p1.b-p2.b),2));
     }
+
 }
