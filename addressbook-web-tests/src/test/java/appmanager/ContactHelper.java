5bbcfec8f96b3b5a5912877330f152099780f37b
akoscynich <a.koscynich@mygento.ru>
2019-01-31 01:25:00 +0300

    caontactCreation list compare by size added

--- addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/appmanager/ContactHelper.java
@@ -3,6 +3,10 @@ package appmanager;
 import data.ContactData;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ContactHelper extends HelperBase{
 
@@ -22,6 +26,7 @@ public class ContactHelper extends HelperBase{
 
     public void submitContactCreation() {
         click(By.name("submit"));
+        find(By.cssSelector("div.msgbox"));
     }
 
     public void selectContact() {
@@ -57,4 +62,46 @@ public class ContactHelper extends HelperBase{
     public boolean isThereAContact() {
         return isElementPresent(By.name("selected[]"));
     }
+
+    public List<ContactData> getContactList() {
+        List<ContactData> contacts = new ArrayList<ContactData>();
+        List<WebElement> rows = wd.findElements(By.name("entry"));//new ArrayList<WebElement>();
+        for (WebElement row : rows) {
+            List<WebElement> cells = row.findElements(By.tagName("td"));
+            String lastName = cells.get(1).getText();
+            String firstName = cells.get(2).getText();
+            ContactData contact = new ContactData(firstName, null, lastName);
+            contacts.add(contact);
+        }
+        return contacts;
+
+        /*public List<GroupData> getGroupList() {
+            List<GroupData> groups = new ArrayList<GroupData>();
+            List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+            for (WebElement element : elements){
+                String name = element.getText();
+                int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+                GroupData group = new GroupData(id, name, null, null);
+                groups.add(group);
+            }
+            return groups;*/
+        /*List<WebElement> elements = wd.findElements(By.cssSelector("span.group"));
+        for (WebElement element : elements){
+            String name = element.getText();
+            int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
+            ContactData contact = new ContactData(id, name, null, null);
+            contacts.add(contact);
+        }
+        return contacts;*/
+    }
+
+    //cells.get(2).getText()
+    /*лучше сделать так:
+            1) найти все строки таблицы
+        2) пройти в цикле по строкам
+        3) внутри цикла каждую строку разбить на ячейки
+         List<WebElement> cells = row.findElements(By.tagName("td"))
+        4) брать из этого списка нужные ячейки по номеру (столбца) и получать их текст*/
+
+
 }
--- addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/tests/ContactCreationTests.java
@@ -1,13 +1,22 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.*;
 import data.ContactData;
+
+import java.util.List;
 
 public class ContactCreationTests extends TestBase {
 
     @Test
     public void testContactCreation() throws Exception {
+        List<ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().createContact(new ContactData("First name", "Middle name", "Last name"));
+        app.getNavigationHelper().gotoHomePage();
+        List<ContactData> after = app.getContactHelper().getContactList();
+        Assert.assertEquals(after.size(), before.size() + 1);
+
+
     }
 
 }
