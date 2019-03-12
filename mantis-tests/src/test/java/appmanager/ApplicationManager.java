d4ff786173c196a136da30974d0624db221dec9e
Anatoly Kiscynich <hamster@node-2.local>
2019-03-12 12:42:42 +0300

    18

--- mantis-tests/src/test/java/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/appmanager/ApplicationManager.java
@@ -65,6 +65,7 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        dbHelper = new DbHelper();
     }
 
     public void stop() {
@@ -94,7 +95,6 @@ public class ApplicationManager {
 
     public WebDriver getDriver() {
         if (wd == null) {
-
             if (browser.equals(BrowserType.FIREFOX)) {
                 wd = new FirefoxDriver();
             } else if (browser.equals(BrowserType.CHROME)) {
