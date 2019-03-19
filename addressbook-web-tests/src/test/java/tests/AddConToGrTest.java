3c7c0c4f096ddc93779a52f90eac794ebcaebb93
Anatoly Kiscynich <hamster@node-2.local>
2019-03-19 17:30:33 +0300

    kjhkhk

--- addressbook-web-tests/src/test/java/tests/AddConToGrTest.java
+++ addressbook-web-tests/src/test/java/tests/AddConToGrTest.java
@@ -4,8 +4,6 @@ import data.*;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-import java.security.acl.Group;
-
 import static org.testng.Assert.assertEquals;
 
 public class AddConToGrTest extends TestBase {
@@ -35,142 +33,69 @@ public class AddConToGrTest extends TestBase {
                     .withHeader("test2")
                     .withFooter("test3"));
         }
-        Contacts contacts = app.db().contacts();
-        Groups groups = app.db().groups();
-        for (ContactData contact : contacts) {
-            if (contact.getGroups().size() < groups.size()) {
-                break;
-            } else {
-                app.goTo().groupPage();
-                app.group().create(new GroupData()
-                        .withName(Long.toString(now * 2))
-                        .withHeader("test2")
-                        .withFooter("test3"));
-            }
-        }
-    }
-
-    @Test
-    public void testAddContactToGroup() {
         for (ContactData contact : app.db().contacts()) {
             if (contact.getGroups().size() < app.db().groups().size()) {
-                Groups before = contact.getGroups();
-                Groups allGroup = app.db().groups();
-                allGroup.remove(before);
-                GroupData group = allGroup.iterator().next();
-                int groupId = group.getId();
-                int contactId = contact.getId();
-                app.goTo().homePage();
-                app.group().selectNew(groupId);
-                app.contact().selectContactById(contactId);
-                app.contact().addToGroup();
-                Groups after = contact.getGroups();
-                assertEquals(before, after.withAdded(group));
                 break;
             } else {
                 app.goTo().groupPage();
                 app.group().create(new GroupData()
-                        .withName(Long.toString(now * 2))
+                        .withName(Long.toString(now))
                         .withHeader("test2")
                         .withFooter("test3"));
-
-                Groups before = contact.getGroups();
-                Groups allGroup = app.db().groups();
-                allGroup.remove(before);
-                GroupData group = allGroup.iterator().next();
-                int groupId = group.getId();
-                int contactId = contact.getId();
-                app.goTo().homePage();
-                app.group().selectNew(groupId);
-                app.contact().selectContactById(contactId);
-                app.contact().addToGroup();
-                Groups after = contact.getGroups();
-                assertEquals(before, after.withAdded(group));
             }
         }
     }
 
-    /*1. В первом тесте надо проверять, что количество групп контакта меньше общего числа групп.
-    Проверять в этом тесте надо не количество групп, а сравнивать списки групп контакта после добавления в группу.*/
-
-    /*2. Во втором тесте не надо вызывать тест добавления контакта в группу,
-    надо просто добавить контакт в группу если контакт не добавлен ни в какую группу.
-    Рекурсивно тест удаления контакта из группы тоже вызывать не надо.
-    Сначала выполняются все необходимые проверки, а затем сам тест.
-    В этом тесте тоже надо сравнивать списки групп контакта после удаления контакта из группы.*/
-
     @Test
-    public void testDelContactFromGroup() {
-        app.goTo().homePage();
-        for (ContactData contact : app.db().contacts())
-            if (contact.getGroups().size() > 0) {
-                Groups before = contact.getGroups();
-                String validGroup = contact.getGroups().iterator().next().getName();
-                GroupData group = contact.getGroups().iterator().next();
-                app.group().selectGroup(validGroup);
-                app.contact().selectContact(contact);
-                app.contact().removeFromGroup();
-                app.goTo().homePage();
-                Groups after = contact.getGroups();
-                assertEquals(before, after.without(group));
-                break;
-            } else {
-                app.goTo().homePage();
-                app.contact().selectContact();
-                app.contact().addToGroup();
-
-                Groups before = contact.getGroups();
-                String validGroup = contact.getGroups().iterator().next().getName();
-                GroupData group = contact.getGroups().iterator().next();
-                app.group().selectGroup(validGroup);
-                app.contact().selectContact(contact);
-                app.contact().removeFromGroup();
-                app.goTo().homePage();
-                Groups after = contact.getGroups();
-                assertEquals(before, after.without(group));
-
-
-            }
-    }
-
-    /*@Test
     public void testAddContactToGroup() {
+        Contacts contacts = app.db().contacts();
+        for (ContactData contact : app.db().contacts())
+            if (contact.getGroups().size() == app.db().groups().size())
+                contacts.remove(contact);
+        ContactData contact = contacts.iterator().next();
+        Groups before = contact.getGroups();
+        Groups allGroup = app.db().groups();
+        allGroup.remove(before);
+        GroupData group = allGroup.iterator().next();
         app.goTo().homePage();
-        int before = app.db().contacts().iterator().next().getGroups().size();
-        if (app.db().groups().size() > 1){
-            app.goTo().groupPage();
-            app.group().create(new GroupData()
-                    .withName(Long.toString(now * 2))
-                    .withHeader("test2")
-                    .withFooter("test3"));
-            app.goTo().homePage();
-            app.group().selectNew(now * 2);}
-        app.contact().selectContact();
+        app.group().selectNew(group.getId());
+        app.contact().selectContactById(contact.getId());
         app.contact().addToGroup();
-        int after = app.db().contacts().iterator().next().getGroups().size();
-
-        assertEquals(before + 1, after);
+        Groups after = contact.getGroups();
+        assertEquals(before, after.withAdded(group));
     }
 
     @Test
     public void testDelContactFromGroup() {
-        app.goTo().homePage();
+        Groups groups = app.db().groups();
+        ContactData validContact = null;
+        GroupData validGroup = null;
+        Groups before = null;
+        int groupId = 0;
         for (ContactData contact : app.db().contacts())
-            if (contact.getGroups().size() > 0) {
-                int before = contact.getGroups().size();
-                String validGroup = contact.getGroups().iterator().next().getName();
-                app.group().selectGroup(validGroup);
-                app.contact().selectContact(contact);
-                app.contact().removeFromGroup();
+            if (contact.getGroups().size() == 0) {
                 app.goTo().homePage();
-                int after = contact.getGroups().size();
-                assertEquals(before - 1, after);
-                break;
+                app.group().selectGroupById(groups.iterator().next().getId());
+                app.contact().selectContactById(contact.getId());
+                app.contact().addToGroup();
+                validContact = contact;
+                validGroup = contact.getGroups().iterator().next();
+                groupId = validGroup.getId();
+                before = contact.getGroups();
             } else {
-                testAddContactToGroup();
-                testDelContactFromGroup();
+                validContact = contact;
+                validGroup = contact.getGroups().iterator().next();
+                groupId = validGroup.getId();
+                before = contact.getGroups();
             }
-    }*/
+        app.goTo().homePage();
+        app.contact().selectContactById(validContact.getId());
+        app.group().selectGroupById(groupId);
+        app.contact().removeFromGroup();
+        Groups after = validContact.getGroups();
+        assertEquals(before, after.withAdded(validGroup));
+    }
+
 
 }
 
